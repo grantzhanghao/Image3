ca3695be53fcf243af873d36aa8dcaa4d3a422de
grantzhang <grantzhang@objectivasoftware.com>
2019-01-03 11:43:31 +0800

    demo

--- djangorest/api/drawgraphs/mock_graph.py
+++ djangorest/api/drawgraphs/mock_graph.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 def draw_mock_graph():
     '''draw the toe energy per meter graph.
     '''
-    plt.plot([1,2,3,4,5, 6], [1,2,4,5,6, 7], 'ro')
+    plt.plot([1,2,3,4,5, 6, 7, 8], [1,2,4,5,6, 7, 10,11], 'ro')
     plt.axis([0, 10, 0, 20 ])    
     canvas = pylab.get_current_fig_manager().canvas
     canvas.draw()
